a0bb721972b1aff4649ea7c17013288d30a8be9f
Cleveland <61059@icf.com>
2023-09-28 15:00:45 -0400

    Adding get_metadata.py file which will input PDF URLs from an Excel workbook and output another workbook with metadata from URLs.

--- get_metadata.py
+++ get_metadata.py
@@ -1,67 +1,131 @@
 import fitz as pdfCrawler # PyMuPDF
-import requests
-import re 
-# from pypdf import PdfReader as pdr # py -m pip install pypdf 
-# from io import BytesIO as stream # used to stream PDF
+import requests as request
+import pandas as pd # https://pandas.pydata.org/docs/getting_started/index.html 
+from datetime import datetime as dt
 
-# to do - implement PDFMetadata class
-class PDFMetaData:
-    def __init__(self, pdfObject):
-        self.format = ""
-        self.title = ""
-        self.author = ""
-        self.subject = ""
-        self.keywords = ""
-        self.creator = ""
-        self.created_date = ""
-        self.mod_date = ""
-        self.fonts = [] # list
-        self.urls = {} # url, whether url is broken or good
-        self.length = 0
-        self.has_img = False
-        self.image_count = 0 
-        self.page_count = 0
+# helper functions 
+def get_pdf(pdf_url):
 
-    def get_page_count(self):
-        return NotImplementedError
+    try:
+        # Download the PDF content from the URL
+        response = request.get(pdf_url)
 
-# get the PDF first
-def get_pdf(response, pdf_url):
+        if response.status_code != 200:
+            print(f"Failed to fetch PDF from {pdf_url} because of {response.status_code}")
+            return
 
-    if response.status_code != 200:
-        print(f"Failed to fetch PDF from {pdf_url}")
-        return
+        # Create a PDF document object
+        pdf_document = pdfCrawler.open(stream=response.content, filetype="pdf")
+        return pdf_document
+    except Exception as err:
+        print(f"Failed to fetch PDF from {pdf_url} because of {err}") 
 
-    # else create a PDF document object
-    pdf_document = pdfCrawler.open(stream=response.content, filetype="pdf")
-    
+def get_metadata(url):
+    # probably could do a lot more more in this method
+    print(pdf_document.metadata)
     return pdf_document.metadata
+
+def get_images(pdf_document): #todo, to implement with PyMuPDF
+    raise NotImplementedError # return dict of "has image bool, count"
+
+def check_url_status(pdf_document, pdf_urls): #todo, to implement with PyMuPDF
+    raise NotImplementedError # todo, return list of dict of key = url, value = url status 
+
+def contains_image(pdf_document): 
+    raise NotImplementedError # to implement with PyMuPDF
+
+def create_excel(metadata_list):
+    # using pandas, create dataframe to hold all the data 
+    metadata_df = pd.DataFrame(metadata_list, columns=["Url", 
+                                                       "Format", 
+                                                       # "File size", # to implement 
+                                                       "Page count", 
+                                                       "Title", 
+                                                        "Date Created", 
+                                                        "Date Modified", 
+                                                        "Author",
+                                                        "Subject",
+                                                        "Keywords",
+                                                       # "URLs"  # i.e., https://www.someurl.com, statusError; https://www.someurl.com, status200; 
+                                                       # "Has Image(s),"  # to implement
+                                                       # "Image count",  # to implement
+                                                       # "Meets 508" # to implement
+                                        ])
+
+    # create excel document
+    # todo add check if file already exists
+    # will save in local folder
+    metadata_df.to_excel(f"PDF_Metadata_{dt.today().strftime('%Y-%m-%d')}.xlsx", sheet_name="PDF_Metadata", index=False)
+
 
 if __name__ == "__main__":
     
-    pdf_url = "https://childcareta.acf.hhs.gov/sites/default/files/new-occ/resource/files/SBRG_Capacity%20Building%20Self%20Assessment%20Tool%202023_0.pdf"  # Replace with your PDF URL
-    all_metadata = [] # pdf, object 
-   
-    # wrap in try / except / finally to catch errors that may happen
+    print(f"Starting process at {dt.today()}")
+    print(f"Grabbing excel file...")
+    
+    # "wrap" your code in try, except, finally to catch errors so that you 1) can debug issues and 2) can ensure your program gracefully exits if it encounters an issue 
     try:
-        # get the response back 
-        response = requests.get(pdf_url)
-        # create new method, "get_pdf" to open the PDF
-        currentPDF = get_pdf(response, pdf_url)
 
-        # now get the metadata from the current PDF
-        # but want to implement a cleaner way to do this later on 
+        # path to the excel file or excel file name if in path
+        path_to_file = "CCTAN-internal_pdfs_20230914.xlsx"
 
-        metadata = {} # dictionary to hold relevant metadata
-        print(currentPDF is dict)
+        # get the excel file from a path and convert to dataframe?
+        excel_df = pd.read_excel(path_to_file)
 
-        for item in currentPDF: # all the values in the metadata dictionary
-            print(item) # print out each item
-      
-    # if there is an error, capture the error message 
-    except Exception as err: 
-    # print the error message so we know what happened and can try to fix the issue= (e.g., typo in the pdf_url)
+        # grab the 2nd row (the row with URLs) and convert to a list 
+        url_list = excel_df.iloc[:,0].values.flatten().tolist()
+        
+        #initalize list to store metadata
+        metadata_list = []
+        missed_urls = []
+
+        # keep track of how many have been iterated so far
+        curr_url_count = 0 
+
+        # iterate over all the URLs in the dataframe
+        for item in url_list:
+            
+            if curr_url_count < 30:  # only look at the first 30 so we can refine 
+                # get the PDF from the URL
+                pdf_document = get_pdf(item)
+
+                # check if nothing came back b/c of error 
+                if pdf_document is None:
+                    print(f"Error with current url: {item}")
+                    missed_urls.append({item})
+                else:
+                    #grab the metadata from the PDF and 
+                    metadata = get_metadata(pdf_document)
+                    #add URL + relevant metadata to list
+                    current_url_metadata = [
+                                            item, 
+                                            metadata["format"], 
+                                            pdf_document.page_count,
+                                            metadata["title"],
+                                            metadata["creationDate"],
+                                            metadata["modDate"],
+                                            metadata["author"],
+                                            metadata["subject"],
+                                            metadata["keywords"],
+                                            ]
+
+                    #append the metadata for the current pdf url to the larger list of all PDFs/urls
+                    metadata_list.append(current_url_metadata)
+
+                curr_url_count += 1 # add 1 more to count 
+
+        # create the final Excel file with all metadata from all PDFs
+        create_excel(metadata_list)
+        print("Created excel file")
+        print("Missed URLs: ")
+
+        for item in missed_urls:
+            print({item})
+
+    except Exception as err:
+        # print the error message so we know what happened and can try to fix the issue= (e.g., typo in the pdf_url)
         print("Error extracting URLs from pdf: ", err)
+    # if there is an error, capture the error message 
     finally:
         print("Done.")
 
